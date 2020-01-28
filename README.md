04f44d4d1d40a31b444c482f0b2bf2de1a4cf812
Agris Mucenieks <agris.mucenieks@gmail.com>
2020-01-28 22:55:20 +0200

    Created decoder base class and decoder for UTF-8

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # TextFileInput
-utility to read text files in c++ with column , row information
+this is c++ class(-es) providing Text Input from UTF-8 Encoded files ( 7-bit ASCII as well).
+- - -
+## Why not standard std::ifstream ?
+although a lot more powerful, but it does not keep trace of position in file expressed in lines and columns. Primary objective of these classes is to keep track of it.
+
+- - -
+
+## When to use this class ?
+In cases where you intend to parse text file and report information about errors , for example reading configuration from file
+
+```
+[Theme]
+Name= ĀžukalnaDubļi
+BackgroundColor=#794c13
+
+
+```
+
+While parsing you encounter symbol "Ā" , but you will accept only ASCII characters, so you would like to create message like 
+`Invalid character 'Ā' in 'settings.ini' Line 2 Column 7. Only alphanumeric ASCII characters are allowed`
+
+Normally you would have to track your location in file. This class does the tracking for you.
+
+In addition to keeping track on row(line) and column information of red characters, this class also provides ability to seek to specific character in file providing it's row(line) and column numbers. 
+
+Note : indexing is 1-based as text editors usually have 1-based indexing
+- - -
+## What do we consider as line end characters ?	
+often we are accustomed to LF or CRLF being used as line end characters , however according to Wikipedia , Unicode standard defines a number of characters that applications should recognize as line terminators:
+- LF ( U+000A )
+- VT ( U+000B )
+- FF ( U+000C )
+- CR ( U+000D )
+- CR+LF ( U+000D 0+000A )
+- NEL ( U+0085 )
+- LS ( U+2028 )
+- PS ( U+2029 )
+* * * *
+
+### Classes and their purposes:
+
+- **TextFileInput_decoder_base** - provides base class for character decoders, thus in future allowing to parse different encodings , not only UTF-8. **Do not direclty use this class**
+- **TextFileInput_decoder_utf8** - provides decoding from UTF-8 to unicode codepoints
+
+### Branches: 
+
+**master** - contains only header files needed, only "tested versions"
+
+**develop** - contains header files and CodeBlocks project for testing purposes
+
+**encodings** - contains development of encoders
---
+++ TextFileInput_decoder_utf8.h
@@ -0,0 +1,103 @@
+#ifndef TEXTFILEINPUT_DECODER_UTF8_H_INCLUDED
+#define TEXTFILEINPUT_DECODER_UTF8_H_INCLUDED
+
+#include <cstdint>
+#include <string>
+#include <limits>
+#include "TextFileInput_decoder_base.h"
+/**
+ * this class provides utility to create decoder between encoding within file and character type that stores unicode codepoint
+ * direct use of this class will result in exception thrown , use decoders derived from it
+ */
+
+template <typename CodepointCharT>
+class TextFileInput_decoder_utf8:public TextFileInput_decoder_base<CodepointCharT>
+{
+	protected:
+        uint_fast32_t m_codepoint;
+		int m_bytesleft;
+	public:
+		TextFileInput_decoder_utf8()=delete;
+		/** construct object and start decoding by loading first byte
+		 * \exception can throw exception to inform that given first byte is invalid std::invalid_argument
+		 */
+		TextFileInput_decoder_utf8(const uint8_t & firstbyte):TextFileInput_decoder_base<CodepointCharT>(firstbyte)
+		{
+			// test if this is valid start byte - starts with 0xxxxxxx ; 110xxxxx; 1110xxxx ; 11110xxx
+            if( (firstbyte&0x80) ==0)
+			{
+				// this is single byte character
+                m_bytesleft=0;
+                m_codepoint=firstbyte;
+			}
+			else if( (firstbyte&0xE0) == 0xC0)
+			{
+				// this is two byte character
+				m_bytesleft=1;
+				m_codepoint=firstbyte&0x1F;
+			}
+			else if ((firstbyte&0xF0) == 0xE0)
+			{
+				// this is three byte character
+				m_bytesleft=2;
+				m_codepoint=firstbyte&0x0F;
+			}
+			else if ((firstbyte&0xF8) == 0xF0)
+			{
+				m_bytesleft=3;
+				m_codepoint=firstbyte&0x07;
+			}
+			else
+			{
+				// invalid byte
+				throw std::invalid_argument("Invalid byte as UTF-8 character start");
+			}
+
+		}
+		/** Check if decoder has received all bytes of text file
+		 * \return true if no more bytes have to be appended , false otherwise
+		 */
+		virtual bool is_complete()const
+		{
+			return m_bytesleft==0;
+		}
+		/** append next byte
+		 * \throw if trying to append invalid byte , std::invalid_argument will be thrown
+		 * \throw if trying to append to complete character , std::logic_error will be thrown
+		 */
+		 virtual void append(const uint8_t &nextbyte)
+		 {
+		 	// check if we are not already done
+		 	if(m_bytesleft<0)
+				throw std::logic_error("Attempting to add too many bytes for UTF-8 character");
+            // check if this byte starts with  10xxxxxx
+            if((nextbyte & 0xC0) != 0x80)
+				throw std::invalid_argument("non-first byte of character in UTF-8 encoding must conform to 10xxxxxx , it didn't");
+			// make space for next 6 bits
+			m_codepoint<<=6;
+			// append 6 bits from this byte
+			m_codepoint |= nextbyte & 0x3F;
+            // update bytes left counter
+            m_bytesleft--;
+		 }
+		 /** get unicode codepoint
+		  * \return returns codepoint in template type
+		  * \throw if character is not complete std::logic_error is thrown
+		  * \throw if codepoint cannot be represented in chosen character type, std::overflow_error
+		  */
+		 virtual CodepointCharT get() const
+		 {
+		 	// test if we can fit this codepoint within target
+		 	if(std::numeric_limits<CodepointCharT>::max() < m_codepoint)
+                throw std::overflow_error("Cannot fit codepoint in target character type(overflow)");
+			/*if(std::numeric_limits<CodepointCharT>::min() > m_codepoint)
+				throw std::underflow_error("Cannot fit codepoint in target character type(underflow)");
+			*/
+			// return value
+			return m_codepoint;
+		 }
+};
+
+
+
+#endif // TEXTFILEINPUT_DECODER_UTF8_H_INCLUDED
